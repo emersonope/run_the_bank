655716249c942936cc4130fb5dea2c79899bf6d9
Emerson <emersonope@gmail.com>
2024-01-19 01:30:38 +1100

    acidionando regra para gerar ag e cc do cliente

--- src/main/java/br/com/runthebank/bankingaccount/utils/AccountUtils.java
+++ src/main/java/br/com/runthebank/bankingaccount/utils/AccountUtils.java
@@ -1,4 +1,31 @@
 package br.com.runthebank.bankingaccount.utils;
 
 public class AccountUtils {
+
+    public static String generateBranchNumber() {
+        int min = 1000;
+        int max = 9999;
+
+        int randomNumber =  (int) Math.floor(Math.random() * (max - min + 1) + min);
+
+        String branchRandomNumber = String.valueOf(randomNumber);
+
+        StringBuilder branchNumber = new StringBuilder();
+
+        return branchNumber.append(branchRandomNumber).toString();
+    };
+
+    public static String generateAccountNumber() {
+        int min = 100000;
+        int max = 999999;
+
+        int randomNumber =  (int) Math.floor(Math.random() * (max - min + 1) + min);
+
+        String accountRandomNumber = String.valueOf(randomNumber);
+
+        StringBuilder accountNumber = new StringBuilder();
+
+        return accountNumber.append(accountRandomNumber).toString();
+    };
+
 }
