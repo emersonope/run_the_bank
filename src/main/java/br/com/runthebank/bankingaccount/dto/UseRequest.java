bd21e7bf5963a76e2a629e3104a27bee0e4875fe
Emerson <emersonope@gmail.com>
2024-01-19 01:14:13 +1100

    criando meus dtos

--- src/main/java/br/com/runthebank/bankingaccount/dto/UseRequest.java
+++ src/main/java/br/com/runthebank/bankingaccount/dto/UseRequest.java
@@ -1,4 +1,24 @@
 package br.com.runthebank.bankingaccount.dto;
 
+import br.com.runthebank.bankingaccount.enums.AccountType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UseRequest {
+    private String firstName;
+    private String lastName;
+    private String gender;
+    private String address;
+    private String stateOfOriging;
+    private String email;
+    private String cpf;
+    private String cnpj;
+    private String phoneNumber;
+    private AccountType accountType;
 }
--- src/main/java/br/com/runthebank/bankingaccount/dto/UserResponse.java
+++ src/main/java/br/com/runthebank/bankingaccount/dto/UserResponse.java
@@ -1,4 +1,21 @@
 package br.com.runthebank.bankingaccount.dto;
 
+import br.com.runthebank.bankingaccount.enums.ResponseCode;
+import br.com.runthebank.bankingaccount.enums.ResponseMessage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserResponse {
+
+    private ResponseCode responseCode;
+    private ResponseMessage responseMessage;
+    private List<AccountInfo> accountInfoList;
 }
