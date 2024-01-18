ea4319a62e8aaaf24c54ad74463f561ebbc04bb8
Emerson <emersonope@gmail.com>
2024-01-19 01:00:55 +1100

    criando metodo createAccount

--- src/main/java/br/com/runthebank/bankingaccount/controller/UserController.java
+++ src/main/java/br/com/runthebank/bankingaccount/controller/UserController.java
@@ -1,4 +1,24 @@
 package br.com.runthebank.bankingaccount.controller;
 
+import br.com.runthebank.bankingaccount.dto.UseRequest;
+import br.com.runthebank.bankingaccount.dto.UserResponse;
+import br.com.runthebank.bankingaccount.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/bankuser")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponse> createAccount(@RequestBody UseRequest useRequest) {
+        return userService.createAccount(useRequest);
+    }
 }
